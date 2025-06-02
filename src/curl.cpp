305bf6b40914195618a9fbd82e762ca3489e030b
loki0b <contato@henriquegarcialeite.com>
2025-06-01 23:04:46 -0300

    update

--- src/curl.cpp
+++ src/curl.cpp
@@ -1,5 +1,5 @@
 #include <string>
-#include "include/curl.hpp"
+#include "curl.hpp"
 
 using std::string;
 
@@ -7,16 +7,10 @@ Curl::Curl() {
     CURLcode code;
 
     code = curl_global_init(CURL_GLOBAL_ALL); //verify code
-    handle = curl_easy_init();
 }
 
 Curl::~Curl() {
-    curl_easy_cleanup(handle);
     curl_global_cleanup();
-}
-
-CURL* Curl::getHandle() const {
-    return handle;
 }
 
 size_t Curl::write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
